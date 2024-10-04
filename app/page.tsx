e896f373054f3680c761233c3835a9558e735ef8
b6540304091 <phiromrat.l@ku.th>
2024-10-04 13:12:22 +0700

    frist commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Hero from "../components/Hero";
-
+// import Hero from "../components/Hero";
+import Mainconnect from "@/components/Mainconnect";
 export default function Home() {
   return (
     <div>
-      <Hero />
+      {/* <Hero /> */}
+      <Mainconnect/>
     </div>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,29 +2,29 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="shadow-lg p-16 mb-5 bg-body rounded">
+    <div className="shadow-lg p-16 mb-5 bg-body rounded bg-blue-200">
       <footer className="footer text-base-content p-10 ">
-        <nav>
+        <nav className="text-black font-bold">
           <h6 className="footer-title">บริการ</h6>
           <a className="link link-hover">ยี่ห้อ</a>
           <a className="link link-hover">ออกแบบ</a>
           <a className="link link-hover">การตลาด</a>
           <a className="link link-hover">โฆษณา</a>
         </nav>
-        <nav>
+        <nav className="text-black font-bold">
           <h6 className="footer-title">มหาวิทยาลัยเกษตรศาสตร์</h6>
           <a className="link link-hover">เกี่ยวกับ</a>
           <a className="link link-hover">ติดต่อ</a>
           <a className="link link-hover">ภารกิจ</a>
           <a className="link link-hover">AI ตรวจวินิจฉัยโรค</a>
         </nav>
-        <nav>
+        <nav className="text-black font-bold">
           <h6 className="footer-title">นโยบาย</h6>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
-        <form>
+        <form className="text-black font-bold">
           <h6 className="footer-title">Newsletter</h6>
           <fieldset className="form-control w-80">
             <label className="label">
